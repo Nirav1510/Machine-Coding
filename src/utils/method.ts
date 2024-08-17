7b2b058d7c6d42a0fbd8f70dea0a1e6eaa8b2de6
Nirav Gusai <nirav.gusai@nobroker.in>
2024-08-18 01:12:05 +0530

    util methods created

---
+++ src/utils/method.ts
@@ -0,0 +1,9 @@
+const debounce = (func: Function, delay: number = 1000) => {
+  let timer: any;
+  return function (this: any, ...args: any) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
