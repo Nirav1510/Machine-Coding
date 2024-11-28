020ed3b614d28001d30e71fa4fada75b61de9faf
Nirav Gusai <gusainirav1@gmail.com>
2024-11-28 23:50:17 +0530

    practice question

--- src/containers/Practice/Random/index.tsx
+++ src/containers/Practice/Random/index.tsx
@@ -1,9 +1,49 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const Child = ({ count }: any) => {
+  // useEffect(() => {
+  //   console.log("child 1");
+  // });
+
+  // useEffect(() => {
+  //   console.log("child 2");
+  // });
+
+  useEffect(() => {
+    console.log("child mount", count);
+
+    return () => {
+      console.log("child unmount", count);
+    };
+  }, []);
+
+  return <div className='text-2xl text-blue-400'>Child</div>;
+};
 
 const Random = () => {
-  return <div>Random</div>;
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("parent");
+  // });
+
+  return (
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-green-400'>{count}</div>
+
+      <button
+        className='bg-red-300 p-4'
+        onClick={() => setCount((prev) => prev + 1)}>
+        Increment
+      </button>
+
+      {/* <Child /> */}
+
+      {count < 3 && <Child count={count} />}
+    </div>
+  );
 };
 
 export default Random;
